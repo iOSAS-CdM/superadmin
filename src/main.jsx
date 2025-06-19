328df2785f329d0b284144cbdf6c7b4e7043320d
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-06-20 01:05:39 +0800

    fix: adjust border radius for improved UI consistency

--- src/main.jsx
+++ src/main.jsx
@@ -45,7 +45,7 @@ const OSAS = () => {
 						colorInfo: rootToHex('var(--primary)'),
 						fontSize: remToPx(1.5),
 						sizeUnit: remToPx(0.5),
-						borderRadius: remToPx(1)
+						borderRadius: remToPx(0.75)
 					}
 				}}
 			>
